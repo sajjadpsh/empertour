e191631c078b80506ee35dcae2ab6816d958c998
Arya Varaste <30382009+varaste@users.noreply.github.com>
2020-03-09 18:40:27 +0330

    AV - last in master

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,36 +1,48 @@
 <template>
   <div class="single-tat stat-v2 stat-fullwidth">
-    <nuxt-link :to="localePath({ name: 'post-id', params: { id: post.id }})">
-      <div class="stat-img">
-        <img src="/img/blog/tat-2.png" alt />
-      </div>
-      <div class="stat-text">
-        <div class="stat-meta">
-          <p>
-            <span class="sm-date">نویسنده</span>
- -
-            <nuxt-link
-              :to="localePath({ name: 'category-name', params: { category: post.category }})"
-            >
-              <span class="sm-category">{{post.category}}</span>
-            </nuxt-link>
-          </p>
-        </div>
+    <div class="stat-img">
+      <img src="/img/blog/tat-2.png" alt />
+    </div>
+    <div class="stat-text">
+      <div class="stat-meta">
+        <p>
+          <span class="sm-date blog-post-author">نویسنده</span> -
 
-        <h4>{{post.title}}</h4>
-        <div class="stat-location-comment">
-          <ul>
-            <li class="slocation city">
-              <i class="fa" aria-hidden="true"></i>
-              {{post.postedAt}}
-            </li>
-            <li class="slocation"></li>
-          </ul>
-        </div>
+          <nuxt-link :to="localePath({ name: 'category-name', params: { category: post.category }})">
+          <span class="sm-category blog-post-category">{{post.category}}</span>
+          </nuxt-link>
+        </p>
       </div>
-    </nuxt-link>
+      <nuxt-link
+        :to="localePath({ name: 'post-id', params: { id: post.id }})"
+      >
+        <h4>{{post.title}}</h4>
+        <p>  اهمیت عدم سفر در این روز ها برای جلوگیری از شیوع هرچه بیشتر ویروس کرونا (بیشن توی خونت شمالیا گناه نکردن ک)</p>
+      </nuxt-link>
+      <div class="stat-location-comment">
+        <ul class="blog-post-date">
+          <li class="slocation city">
+            <i class="fa" aria-hidden="true"></i>
+            {{post.postedAt}}
+          </li>
+          <li class="slocation"></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .blog-post-author{
+    color:#6a1b9a;
+  }  
+  .blog-post-category{
+    color:#ffc107;
+  }
+  .blog-post-date{
+    color:#ffc107;
+  }
+</style>
 
 <script>
 export default {
